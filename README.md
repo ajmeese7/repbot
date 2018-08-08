7c11f91d9fa8f2a1e335a52eb87b4853374679e5
Aaron Meese <ajmeese7@gmail.com>
2018-08-08 11:26:22 -0600

    Success
    Now it works once, after being provided with the correct login credentials. Doesn't loop every 12 hours yet

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # repbot
 Bot for artificially influencing rankings on discordbots.org
+
+### Usage
+To use the program, go to [discordbots.org](discordbots.org) and find the bot
+you want to upvote. Once you are on the bot's page, get its ID from the URL.
+
+In the below link, the ID is `159985870458322944`, which belongs to mee6:
+[http://discordbots.org/bot/159985870458322944/vote](http://discordbots.org/bot/159985870458322944/vote).
+
+Once you have copied the ID, paste it as the value for the `bot` variable in
+`start.js`. You must also paste your Discord login credentials in the `email`
+and `password` variables. As this is open source, you can tell that there is no
+possible way that I can steal your login information, so don't freak out.
--- start.js
+++ start.js
@@ -1,23 +1,32 @@
 const Nightmare = require('nightmare');
 var nightmare = new Nightmare({show: true, typeInterval: 75, pollInterval: 250});
+var bot = "159985870458322944"; // The discordbots.org ID of the bot you want to upvote
+// IDEA: CLI with bot ID?
 
-var email = "aaron";
-var password = "0123456789";
+var email = "your@email.com";
+var password = "y0uRpAsswRdHERE";
 
 // IDEA: Add option to cycle through multiple accounts via array/txt doc
 // NOTE: Creating accounts is probably out of scope. Different project / post other?
+//       TODO: TEST WITH TEMP EMAIL!
 run();
 
 function run() {
   nightmare
-    .goto('http://discordbots.org/bot/159985870458322944/vote')
+    .goto('http://discordbots.org/bot/' + bot + '/vote')
     .wait('#votingvoted')
-    .click('#votingvoted') // Vote for bot
+    .click('#votingvoted') // Click vote for bot button
     .wait('.modal-content')
     .click('.modal-content p a') // Login to vote
     .type('form div:nth-child(1) div input', email) // Type email
     .type('form div:nth-child(2) div input', password) // Type password
-    .wait(30000)
+    .wait(3000)
+    .click('[type="submit"]') // Click login button
+    .wait('.primary')
+    .click('.primary') // Click authorize button
+    .wait('#votingvoted')
+    .click('#votingvoted') // Vote for bot
+    .wait(3000)
     .end() // For some reason it won't run without the end/then
     .then((result) => {
       console.log("Done.");
